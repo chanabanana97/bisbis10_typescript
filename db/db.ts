8276bdcb7f854c589c2497ba76f1134437a6617c
chanabanana97 <dalfenchana@gmail.com>
2024-05-12 16:46:38 +0300

    db connection

--- db/db.ts
+++ db/db.ts
@@ -16,4 +16,25 @@ client.connect((err: Error) => {
   }
 });
 
-export default client;
+
+exports.executeQuery = async function(query: string, params: any[]) {
+  try {
+    const result = await client.query(query, params);
+    return result.rows;
+  } catch (err) {
+    console.error("Error executing query", err);
+    throw err;
+  }
+}
+
+// // Update operation
+// export async function updateTable(tableName: string, id: any, updates: Record<string, any>) {
+//   const fields = Object.keys(updates).filter(field => field !== 'id'); // Exclude 'id' field
+//   const setClause = fields.map((field, index) => `${field} = COALESCE($${index + 1}, ${field})`).join(', ');
+//   const query = `UPDATE ${tableName} SET ${setClause} WHERE id = $${fields.length + 1} RETURNING id`;
+//   const values = [...Object.values(updates), id];
+//   return exec(query, values);
+// }
+
+
+exports.client = client;
--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,30 @@
-CREATE TABLE example (
+CREATE TABLE IF NOT EXISTS restaurants (
     id SERIAL PRIMARY KEY,
-    data VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    averageRating NUMERIC(4,2),
+    isKosher BOOLEAN,
+    cuisines TEXT[]
 );
+
+CREATE TABLE IF NOT EXISTS dishes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2) NOT NULL,
+    UNIQUE (name, description, price)
+);
+
+CREATE TABLE IF NOT EXISTS restaurant_dishes (
+    restaurantId INT,
+    dishId INT,
+    PRIMARY KEY (restaurantId, dishId),
+    FOREIGN KEY (restaurantId) REFERENCES restaurants(id) ON DELETE CASCADE,
+    FOREIGN KEY (dishId) REFERENCES dishes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+    restaurantId INT,
+    rating NUMERIC(4,2),
+    FOREIGN KEY (restaurantId) REFERENCES restaurants(id) ON DELETE CASCADE
+
+)

525d68bd56b1c8776dbdb98d6613cd53e8edad2d
chanabanana97 <dalfenchana@gmail.com>
2024-05-16 15:05:32 +0300

    add env variables

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,14 @@
 import { Client } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const client = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "example",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 client.connect((err: Error) => {
